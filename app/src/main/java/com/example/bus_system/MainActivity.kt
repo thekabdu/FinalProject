a9d622671ab703d6703937e06d3d35d6f149a4b4
Abylai <aboka_99@mail.ru>
2020-12-03 18:51:37 +0600

    menu

--- app/src/main/java/com/example/bus_system/MainActivity.kt
+++ app/src/main/java/com/example/bus_system/MainActivity.kt
@@ -3,18 +3,45 @@ package com.example.bus_system
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ActionMenuView
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     lateinit var auth: FirebaseAuth
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val logout = findViewById<Button>(R.id.logout_btn)
+       toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0,0
+        )
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener (this)
+
+        //val logout = findViewById<Button>(R.id.logout_btn)
 
         auth= FirebaseAuth.getInstance()
         var currentUser=auth.currentUser
@@ -24,11 +51,37 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
-        logout.setOnClickListener{
+      /*  logout.setOnClickListener{
             auth.signOut()
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
+        }*/
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_signOut -> {
+
+            }
         }
 
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
